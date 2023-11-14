83bb05bdcf6f1996d0c461048526d8c2f574a1c5
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2023-11-14 15:41:42 -0500

    Update advanced_search_tips.js

--- js/advanced_search_tips.js
+++ js/advanced_search_tips.js
@@ -3,6 +3,5 @@
  * Handles Ajax submission / updating form action on url change, etc.
  */
 (function ($, Drupal, drupalSettings) {
-    console.log("testing");
-    jQuery('<span id="search-help"><button type="button" class="btn btn-link dsu" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa-solid fa-circle-info"></i></button></span>').insertAfter(".advanced-search-form__search");
+
 })(jQuery, Drupal, drupalSettings);
