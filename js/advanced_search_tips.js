14200ee3686260630d7869837309a6be6a3dd221
Kyle Huynh <kyle.huynh205@gmail.com>
2023-11-14 14:21:15 -0500

    Restructure module to avoid asset injectors

---
+++ js/advanced_search_tips.js
@@ -0,0 +1,8 @@
+/**
+ * @file
+ * Handles Ajax submission / updating form action on url change, etc.
+ */
+(function ($, Drupal, drupalSettings) {
+    console.log("testing");
+    jQuery('<span id="search-help"><button type="button" class="btn btn-link dsu" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa-solid fa-circle-info"></i></button></span>').insertAfter(".advanced-search-form__search");
+})(jQuery, Drupal, drupalSettings);
